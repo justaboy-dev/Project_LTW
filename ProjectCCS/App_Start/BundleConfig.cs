693faa0182971e980b4f6cdd3168cf56d5e07c5b
justaboy-dev <nhattan.thsv@gmail.com>
2021-06-02 20:24:52 +0700

    full source

--- ProjectCCS/App_Start/BundleConfig.cs
+++ ProjectCCS/App_Start/BundleConfig.cs
@@ -22,8 +22,7 @@ namespace ProjectCCS
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/app/js/app.js",
-                      "~/app/js/owl.carousel.min.js",
-                      "~/app/js/jquery.min.js"));
+                      "~/app/js/owl.carousel.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/app/dist/css/style.css"));
